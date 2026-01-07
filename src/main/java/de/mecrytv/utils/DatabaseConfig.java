88e971f9871a9a768feed08e01451aa2de388087
MecryTv <Development@mecrytv.de>
2026-01-07 23:11:32 +0100

    [INI] Initial Commit

--- src/main/java/de/mecrytv/utils/DatabaseConfig.java
+++ src/main/java/de/mecrytv/utils/DatabaseConfig.java
@@ -1,6 +1,6 @@
 package de.mecrytv.utils;
 
 public record DatabaseConfig(
-        String mariaHost, int mariaPort, String mariaDatabase, String mariaUser, String mariaPassword,
+        String mariaHost, int mariaPort, String mariaDatabase, String mariaUsername, String mariaPassword,
         String redisHost, int redisPort, String redisPassword
 ) {}
