c8029a4d78232957aceaaf428464b58e25bd4f13
MecryTv <Development@mecrytv.de>
2026-01-09 19:41:57 +0100

    [EDIT] Redis Lettuce ClassNotFound Fix

--- src/main/java/de/mecrytv/redis/RedisManager.java
+++ src/main/java/de/mecrytv/redis/RedisManager.java
@@ -6,7 +6,6 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.resource.DefaultClientResources;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.Set;
 
@@ -19,7 +18,10 @@ public class RedisManager {
         System.setProperty("io.lettuce.core.epoll", "false");
         System.setProperty("io.lettuce.core.kqueue", "false");
 
-        ClientResources res = DefaultClientResources.create();
+        ClientResources res = DefaultClientResources.builder()
+                .ioThreadPoolSize(4)
+                .computationThreadPoolSize(4)
+                .build();
 
         String auth = config.redisPassword().isEmpty() ? "" : ":" + config.redisPassword() + "@";
         String url = "redis://" + auth + config.redisHost() + ":" + config.redisPort();
@@ -29,24 +31,15 @@ public class RedisManager {
         this.async = connection.async();
     }
 
-    public CompletableFuture<String> get(String key) {
-        return async.get(key).toCompletableFuture();
-    }
-    public void set(String key, String val) {
-        async.set(key, val);
-    }
-    public void setex(String key, long seconds, String value) {
-        async.setex(key, seconds, value);
-    }
-    public void sadd(String key, String member) {
-        async.sadd(key, member);
-    }
-    public void srem(String key, String member) {
-        async.srem(key, member);
-    }
+    public CompletableFuture<String> get(String key) { return async.get(key).toCompletableFuture(); }
+    public void set(String key, String val) { async.set(key, val); }
+    public void setex(String key, long seconds, String value) { async.setex(key, seconds, value); }
+    public void sadd(String key, String member) { async.sadd(key, member); }
+    public void srem(String key, String member) { async.srem(key, member); }
     public CompletableFuture<Set<String>> smembers(String key) {
         return async.smembers(key).toCompletableFuture().thenApply(java.util.HashSet::new);
     }
+
     public void disconnect() {
         connection.close();
         client.shutdown();
